e3fe426a1b08d9914389d1e56e42bbac0611eddc
Chanseok Oh <chanseok@google.com>
2018-09-05 13:20:07 -0400

    wip

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -7,15 +7,21 @@ set PATH=%JAVA_HOME%\bin;%PATH%
 cd github/jib
 
 REM Stops any left-over containers.
-REM FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker rm -vf %%i
+FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker rm -vf %%i
 
 REM Sets the integration testing project.
 set JIB_INTEGRATION_TESTING_PROJECT=jib-integration-testing
 
-REM TODO: Enable integration tests once docker works (b/73345382).
-cd jib-core && call gradlew.bat clean build --info --stacktrace && ^
+set CLOUDSDK_CORE_DISABLE_USAGE_REPORTING=true
+call gcloud.cmd components install docker-credential-gcr
+@echo on
+
+set GOOGLE_APPLICATION_CREDENTIALS=%KOKORO_KEYSTORE_DIR%\72743_jib_integration_testing_key
+docker-credential-gcr configure-docker
+
+cd jib-core && call gradlew.bat clean build integrationTest --info --stacktrace && ^
 cd ../jib-plugins-common && call gradlew.bat clean build --info --stacktrace && ^
-cd ../jib-maven-plugin && call mvnw.cmd clean install -B -U -X && ^
-cd ../jib-gradle-plugin && call gradlew.bat clean build --info --stacktrace
+cd ../jib-maven-plugin && call mvnw.cmd clean install -P integration-tests -B -U -X && ^
+cd ../jib-gradle-plugin && call gradlew.bat clean build integrationTest --info --stacktrace
 
 exit /b %ERRORLEVEL%
