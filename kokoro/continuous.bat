d94c6af1f10e03a939833a955ac72c448a766a8c
Chanseok Oh <chanseok@google.com>
2018-09-10 15:57:37 -0400

    wip

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -85,6 +85,8 @@ REM ls -al "/cygdrive/c/Program Files/Docker/Docker/resources/"
 
 CMD /C START dockerd --experimental
 
+sleep 30s
+
 Tasklist
 
 docker version
@@ -95,6 +97,13 @@ docker pull registry:2
 docker pull --platform linux registry:2
 
 tasklist
+
+:CheckDockerUp
+docker info
+IF ERRORLEVEL 1 (
+  sleep 3s
+  GOTO CheckDockerUp
+)
 
 REM REM docker-credential-gcr uses GOOGLE_APPLICATION_CREDENTIALS as the credentials key file
 REM set GOOGLE_APPLICATION_CREDENTIALS=%KOKORO_KEYSTORE_DIR%\72743_jib_integration_testing_key
